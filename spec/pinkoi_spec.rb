4bcf23f8203eef5f781f9d1ecd6e30aaf2fc4c11
frank1234211 <frank1234211@gmail.com>
2015-10-11 15:36:27 +0800

    crete code for test and add minitest-rg into GemfIle

---
+++ spec/pinkoi_spec.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+require_relative '../lib/parameter_parser'
+require_relative '../lib/scrape'
+
+base_url = "http://www.pinkoi.com/browse?"
+scraper = Scraper::Filter.new
+
+items_from_file = YAML.load(File.read('../spec/testfiles/items.yml'))
+items_found = scraper.fetch_result("http://www.pinkoi.com/browse?category=2")
+
+
+describe 'get itmes form category2' do 
+	items_from_file['category2'].each do |item_from_file|
+		#item_from_file.keys has only one key which is item's title
+		#and has only one value which is item's price
+		it "finds '#{item_from_file.keys}' item" do
+			items_found.each do |item_found|
+				#search for the same title name ,and then compare the prices
+				if item_found[:title] == item_from_file.keys
+					item_found[:price].must_equal item_from_file.values
+				end
+			end
+		end
+	end
+end
